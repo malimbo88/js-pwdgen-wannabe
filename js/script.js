25aa2004e0ea84462dc8e1e3e8f3ff3287481ddb
Nicola Valente <nicolavalente88@gmail.com>
2020-05-19 17:40:51 +0200

    Corretto?

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 // MyJavaScript
-var nome = prompt("Qual e'il tuo nome?");
-var cognome = prompt("E il tuo cognome?");
-var colore = prompt("Qual e'il tuo colore preferito?");
-var eta = prompt("Quanti anni hai?")
-var annoDiNascita = 2020 - eta;
-var informazioniUtente = "Ciao! " + nome + " " + cognome + " " + "Classe" + " " + annoDiNascita + " " + colore + " " + "19";
-console.log(informazioniUtente);
+var nome = prompt("Qual é il tuo nome?");
+var saluto =  "Ciao! " + nome;
+var cognome = prompt("Qual e'il tuo cognome?");
+var colore = prompt("Qual é il tuo colore preferito?")
+var informazioniUtente = nome + cognome + colore + "19";
 document.getElementById("user_info").innerHTML = informazioniUtente;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var nome = prompt("Qual e'il tuo nome?");
+//var cognome = prompt("E il tuo cognome?");
+//var colore = prompt("Qual e'il tuo colore preferito?");
+//var eta = prompt("Quanti anni hai?")
+//var annoDiNascita = 2020 - eta;
+//var informazioniUtente = "Ciao! " + nome + " " + cognome + " " + "Classe" + " " + annoDiNascita + " " + colore + " " + "19";
+//console.log(informazioniUtente);
+//document.getElementById("user_info").innerHTML = informazioniUtente;
