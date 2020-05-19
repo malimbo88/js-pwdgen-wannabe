575bf095e4937a6b1a42be745643cb3c5cbe0687
Nicola Valente <nicolavalente88@gmail.com>
2020-05-19 16:26:23 +0200

    Esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 // MyJavaScript
-var nome = prompt("Ciao. Come ti chiami?");
-console.log(nome)
+var nome = prompt("Qual e'il tuo nome?");
+var cognome = prompt("E il tuo cognome?");
+var colore = prompt("Qual e'il tuo colore preferito?");
+var eta = prompt("Quanti anni hai?")
+var annoDiNascita = 2020 - eta;
+var informazioniUtente = "Ciao! " + nome + " " + cognome + " " + "Classe" + " " + annoDiNascita + " " + colore + " " + "19";
+console.log(informazioniUtente);
+document.getElementById("user_info").innerHTML = informazioniUtente;
