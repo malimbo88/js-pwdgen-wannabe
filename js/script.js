258213679aad487e96b65b157ce8207b16af5262
Nicola Valente <nicolavalente88@gmail.com>
2020-05-19 17:42:43 +0200

    Ecco

--- js/script.js
+++ js/script.js
@@ -5,31 +5,3 @@ var cognome = prompt("Qual e'il tuo cognome?");
 var colore = prompt("Qual é il tuo colore preferito?")
 var informazioniUtente = nome + cognome + colore + "19";
 document.getElementById("user_info").innerHTML = informazioniUtente;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var nome = prompt("Qual e'il tuo nome?");
-//var cognome = prompt("E il tuo cognome?");
-//var colore = prompt("Qual e'il tuo colore preferito?");
-//var eta = prompt("Quanti anni hai?")
-//var annoDiNascita = 2020 - eta;
-//var informazioniUtente = "Ciao! " + nome + " " + cognome + " " + "Classe" + " " + annoDiNascita + " " + colore + " " + "19";
-//console.log(informazioniUtente);
-//document.getElementById("user_info").innerHTML = informazioniUtente;
